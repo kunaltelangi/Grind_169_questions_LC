d6c528c947565bb076ee80c0976e8b2cd6519a7a
Kunal Telangi <74617472+kunaltelangi@users.noreply.github.com>
2023-09-08 18:51:04 +0530

    Update Two Sum.cpp.cpp

--- Two Sum.cpp.cpp
+++ Two Sum.cpp.cpp
@@ -1,13 +1,17 @@
+#include <unordered_map>
+ 
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        for (int i = 0; i < nums.size(); i++) {
-            for (int j = i + 1; j < nums.size(); j++) {
-                if (nums[i] + nums[j] == target) {
-                    return {i, j};
-                }
-            }
+        unordered_map<int, int> mp;
+       
+        for(int i = 0; i < nums.size(); i++){
+            if(mp.find(target - nums[i]) == mp.end())
+                mp[nums[i]] = i;
+            else
+                return {mp[target - nums[i]], i};
         }
-        return {};
+ 
+        return {-1, -1};
     }
 };
